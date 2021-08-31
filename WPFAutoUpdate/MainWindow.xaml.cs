97bae7cd642dbb17870f39f19bf3a304151a9036
Mayron Rodrigues de Almeida <mayronrjbr@gmail.com>
2021-08-31 14:33:11 -0300

    Adicionar arquivos de projeto.

---
+++ WPFAutoUpdate/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPFAutoUpdate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Assembly assembly = Assembly.GetEntryAssembly();
+            LabelVersion.Content = $"Current Version : {assembly.GetName().Version}";
+            Thread.CurrentThread.CurrentCulture =
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("pt-BR");
+            AutoUpdater.LetUserSelectRemindLater = true;
+            AutoUpdater.RemindLaterTimeSpan = RemindLaterFormat.Minutes;
+            AutoUpdater.RemindLaterAt = 1;
+            AutoUpdater.ReportErrors = true;
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMinutes(2) };
+            timer.Tick += delegate { AutoUpdater.Start("http://rbsoft.org/updates/AutoUpdaterTestWPF.xml"); };
+            timer.Start();
+        }
+
+        private void ButtonCheckForUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            AutoUpdater.Start("http://rbsoft.org/updates/AutoUpdaterTestWPF.xml");
+        }
+    }
+}
